91151b02e88a011dc8c62d0244dd3094885b4eac
EC2 Default User <ec2-user@ip-172-31-38-198.eu-west-2.compute.internal>
2020-01-25 09:23:38 +0000

    Completed Session 1

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -5,4 +5,13 @@
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+    
+    new_string = ""
+    string.split('').each.with_index { |c,n| new_string += string[n+1] if n + 1 < string.length && c.upcase == "R" }.join
+    new_string
+    
 end
+
+puts pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
+puts pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
+puts pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"

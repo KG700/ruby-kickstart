e3262f9c17c641dc7b50ee2f28d92d19562b8342
EC2 Default User <ec2-user@ip-172-31-38-198.eu-west-2.compute.internal>
2020-02-02 13:45:36 +0000

    Completed Session 2: Question 4

--- session2/challenge/4_array.rb
+++ session2/challenge/4_array.rb
@@ -7,3 +7,16 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
+
+def get_squares(numbers)
+    sorted_array = []
+    numbers.each do |num1|
+      numbers.each do |num2|
+        if num1 * num1 == num2
+          sorted_array.push(num1)
+          break
+        end
+      end
+    end
+  sorted_array.sort
+end

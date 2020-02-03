aa3390afd26e4fa8781e9113a9a33700f51e00d1
EC2 Default User <ec2-user@ip-172-31-38-198.eu-west-2.compute.internal>
2020-02-03 17:04:13 +0000

    Completed Session 2: Question 12

--- session2/challenge/12_classes.rb
+++ session2/challenge/12_classes.rb
@@ -21,8 +21,30 @@
 # f.to_f               # => 0.5
 
 class Fraction
+  
+  def initialize(numerator, denominator)
+    @numerator = numerator
+    @denominator = denominator
+  end
+
+  attr_accessor 'numerator', 'denominator'
+  
   def gcd(a,b)
     return a if b == 0
     gcd(b, a%b)
   end
+  
+  def to_f
+    @numerator.to_f / @denominator.to_f
+  end
+  
+  def lowest
+    common_denominator = gcd(@numerator,@denominator)
+    Fraction.new(@numerator/common_denominator, @denominator/common_denominator)
+  end
+
+  def to_s
+    "#{@numerator}/#{@denominator}"
+  end
+  
 end

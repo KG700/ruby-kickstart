c0656c9185fc88ff7bfa57e5217dc3d72ca78eb7
EC2 Default User <ec2-user@ip-172-31-38-198.eu-west-2.compute.internal>
2020-02-03 11:46:15 +0000

    Completed Session 2: Question 11

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -18,3 +18,45 @@
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+class BeerSong
+  
+  def initialize(number)
+    @number = number
+    @number = 0 if number <= 0
+    @number = 99 if number >= 99
+  
+  end
+  
+  def print_song
+    
+    number = @number
+    
+    while number > 0
+ 
+      puts "#{beers(number).capitalize} #{bottle(number)} of beer on the wall,"
+      puts "#{beers(number).capitalize} #{bottle(number)} of beer,"
+      puts "Take one down, pass it around,"
+      number -= 1
+      puts "#{beers(number).capitalize} #{bottle(number)} of beer on the wall."
+    
+    end
+    
+  end
+  
+  def beers(number)
+
+    ones = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine","ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+    tens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+    
+    num_beer = ones[number] if number < 20
+    num_beer = tens[number / 10 - 2] if number >= 20
+    num_beer = num_beer + "-#{ones[number % 10]}" if number >= 20 && number % 10 > 0
+    
+    return num_beer
+  end
+  
+  def bottle(number)
+    number == 1? bottles = "bottle" : bottles = "bottles"
+  end
+end
